8bd84778a6b96c2584ccc8ba203da30869fe3b1b
Tarric Sookdeo <tarricsookdeo@outlook.com>
2023-04-20 01:46:11 -0400

    Update model and template

--- activities/tasks.py
+++ activities/tasks.py
@@ -9,13 +9,22 @@ logger = get_task_logger(__name__)
 
 @shared_task
 def update_activity():
+    # Get the first activity, we only have one
     activity = Activity.objects.first()
+
+    # Get the activity from the Bored API
     response = requests.get('http://www.boredapi.com/api/activity/')
     data = response.json()
+
+    # Update the activity
     activity.activity = data['activity']
     activity.type = data['type']
     activity.participants = data['participants']
     activity.price = data['price']
     activity.link = data['link']
+
+    # Save the activity
     activity.save()
+
+    # Log that the activity was updated
     logger.info('Activity updated')
